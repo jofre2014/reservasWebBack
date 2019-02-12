a4cef547bb8d29778888e5bf5010d4d8e1bca48c
jose <jose.jofre84@gmail.com>
2019-02-12 16:56:03 -0300

    Confirmar Reserva.

--- src/main/java/com/springboot/app/controllers/ReservaRestController.java
+++ src/main/java/com/springboot/app/controllers/ReservaRestController.java
@@ -1,12 +1,19 @@
 package com.springboot.app.controllers;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,70 +29,68 @@ import com.springboot.app.models.entity.Reserva;
 import com.springboot.app.models.service.IReservaService;
 
 /**
-*
-* @author Jose
-*/
+ *
+ * @author Jose
+ */
 @RestController
 @RequestMapping("api")
 public class ReservaRestController {
-	
-	@Autowired IReservaService iReservaService;
-	
+
+	@Autowired
+	IReservaService iReservaService;
+
 	@PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')")
-    @GetMapping(value = "/reserva")
-    public List<Reserva> listar() {
-        return iReservaService.findAll();
-    }
-	
+	@GetMapping(value = "/reserva")
+	public List<Reserva> listar() {
+		return iReservaService.findAll();
+	}
+
 	@PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')")
 	@PostMapping("/reservas")
 	public Boolean generarReserva(@RequestBody List<ReservaDTO> reserva) {
 		System.out.println("en el restController");
-		return iReservaService.generarReserva(reserva);		
+		return iReservaService.generarReserva(reserva);
 	}
-	
+
 	@PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')")
 	@GetMapping("/reservas/confirmadas/{cliente}/{estado}/{page}")
-	public Page<Reserva> listarReservasEstadoConfir(@PathVariable int cliente, @PathVariable short estado, @PathVariable Integer page){
+	public Page<Reserva> listarReservasEstadoConfir(@PathVariable int cliente, @PathVariable short estado,
+			@PathVariable Integer page) {
 		return iReservaService.findConfirmadaXCliente(cliente, estado, PageRequest.of(page, 2));
 	}
-	
+
 	@PreAuthorize("isAuthenticated() and hasRole('ROLE_ADMIN')")
 	@PutMapping("/reservas/confirmarReserva/{reservaID}")
-	public Boolean confirmarReserva(@PathVariable int reservaID) {
-		System.out.println("Id de reservaaa!!!" + reservaID );
+	public ResponseEntity<?> confirmarReserva(@PathVariable int reservaID) {
+		Map<String, Object> response = new HashMap<>();
+		
 		Reserva updateReserva = iReservaService.findById(reservaID);
 		
-		System.out.println("reserva: " + updateReserva);
-		
-		ObjectMapper mapperObj = new ObjectMapper();
-		try {
-			String jsonStr = mapperObj.writeValueAsString(updateReserva);
-
-			System.out.println(jsonStr);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				
+		if (updateReserva == null) {
+			response.put("mensaje", "Error: no se pudo confirmar la reserva, reserva ID: "
+					.concat(((Integer)reservaID).toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
 		}
 		
-		
+		try {
+			
 		updateReserva.setConfirmada((short) 1);
 		updateReserva.setPendiente((short) 0);
 		updateReserva.setUpdated(new Date());
-		
-		
-	
-		try {
-		Reserva reservaActualizaa =  iReservaService.save(updateReserva);
-		System.out.println("reservaActualizada: " + reservaActualizaa);
-		}catch(Exception e) {
-			e.printStackTrace();
-			return false;
+			
+		Reserva reservaActualizaa = iReservaService.save(updateReserva);
+
+		} catch (DataAccessException e) {
+			response.put("mensaje", "Error al confirmar reserva en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+
+		response.put("mensaje", "La reserva ha sido confirmada con éxito!");
+		response.put("reserva", updateReserva);
 		
-		
-		return true;
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
-	
 
 }
